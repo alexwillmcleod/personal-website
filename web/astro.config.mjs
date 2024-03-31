45cae8ab6f61a61eb3f28f5dde9617db90d293a8
Alexander McLeod <alexwillmcleod@gmail.com>
2024-03-31 19:27:33 +1300

    added outdir to config

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alexmcleod.netlify.app',
+  outDir: "./dist",
   integrations: [mdx(), sitemap(), tailwind()],
   output: "static",
   adapter: netlify()
