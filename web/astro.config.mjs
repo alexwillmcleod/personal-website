c83ae5810496cae2061605370c112fbd4690ad2c
Alexander McLeod <alexwillmcleod@gmail.com>
2024-03-31 19:17:03 +1300

    readded site in the config to fix error

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -6,6 +6,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://alexmcleod.netlify.app',
   integrations: [mdx(), sitemap(), tailwind()],
   output: "static",
   adapter: netlify()
