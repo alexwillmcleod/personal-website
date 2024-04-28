c46e22e5a62590b88ec391787744d0c528bbb479
clear <alexwillmcleod@gmail.com>
2024-04-28 17:35:42 +1200

    changed title from xander mcleod's website to just xander mcleod

--- web/src/consts.ts
+++ web/src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Xander McLeod's Website";
+export const SITE_TITLE = 'Xander McLeod';
 export const SITE_DESCRIPTION =
   'Xander McLeod is a final year computer science student at Waipapa Taumata Rau the University of Auckland';
