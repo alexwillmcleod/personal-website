55cb6deac2f469671182eacbb6155365ccf12da1
clear <alexwillmcleod@gmail.com>
2024-04-28 13:41:57 +1200

    added site description

--- web/src/consts.ts
+++ web/src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Alex McLeod's Website";
-export const SITE_DESCRIPTION = '';
+export const SITE_TITLE = "Xander McLeod's Website";
+export const SITE_DESCRIPTION =
+  'Final Year Computer Science Student at Waipapa Taumata Rau the University of Auckland';
